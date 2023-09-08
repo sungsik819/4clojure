19517896426e26793014c78a7f05a552c95c05b1
sungsik.kim <skystar819@gmail.com>
2023-09-09 07:55:56 +0900

    update problem 2

--- src/problem2.clj
+++ src/problem2.clj
@@ -1,4 +1,8 @@
 (ns problem2)
 
-;; problem 2 - Simple Math
-(= (- 10 (* 2 3)) (* 2 2))
+;; problem 02 - Simple Math
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-02/
+
+;; (= (- 10 (* 2 3)) __)
+
+(= (- 10 (* 2 3)) 4)
