df7f79950d1b19b3f3a4dae59bf5ad0243f44e6a
sungsik.kim <skystar819@gmail.com>
2023-09-10 07:13:26 +0900

    update problem 3

--- src/problem3.clj
+++ src/problem3.clj
@@ -1,4 +1,6 @@
 (ns problem3)
 
-;; problem 3 - Intro to Strings
+;; problem 03 - Intro to Strings
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-03/
+
 (= "HELLO WORLD" (.toUpperCase "hello world"))
