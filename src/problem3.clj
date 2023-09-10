0590994cd50e270c4844fbb0a8cac339ecb2ec8b
sungsik.kim <skystar819@gmail.com>
2023-09-11 05:22:17 +0900

    update problem 3, 4

--- src/problem3.clj
+++ src/problem3.clj
@@ -3,4 +3,6 @@
 ;; problem 03 - Intro to Strings
 ;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-03/
 
+;; (= __ (.toUpperCase "hello world"))
+
 (= "HELLO WORLD" (.toUpperCase "hello world"))
--- src/problem4.clj
+++ src/problem4.clj
@@ -1,5 +1,8 @@
 (ns problem4)
 
-;; problem 4 - Intro to Lists
+;; problem 04 - Intro to Lists
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-04/
+
 ;; (= (list __) '(:a :b :c))
+
 (= (list :a :b :c) '(:a :b :c))
