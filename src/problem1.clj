567507d685b1e984fd344e93e3518901b322a60e
sungsik.kim <skystar819@gmail.com>
2023-09-12 05:35:38 +0900

    update problem 5

--- src/problem1.clj
+++ src/problem1.clj
@@ -1,6 +1,6 @@
 (ns problem1)
 
-;; problem 1 - Nothing but the Truth
+;; problem 01 - Nothing but the Truth
 ;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-01/
 
 ;; (= (__ true))
--- src/problem5.clj
+++ src/problem5.clj
@@ -1,7 +1,10 @@
 (ns problem5)
 
-;; problem 5 - conj on lists
+;; problem 05 - conj on lists
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-05/
+
 ;; (= __ (conj '(2 3 4) 1))
 ;; (= __ (conj '(3 4) 2 1))
+
 (= '(1 2 3 4) (conj '(2 3 4) 1))
 (= '(1 2 3 4) (conj '(3 4) 2 1))
