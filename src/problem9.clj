8b9145119b1bdab5525028133a485da38479f041
sungsik.kim <skystar819@gmail.com>
2023-09-16 15:28:49 +0900

    update problem 9

--- src/problem9.clj
+++ src/problem9.clj
@@ -1,6 +1,8 @@
 (ns problem9)
 
-;; problem 9 - Sets: conj
+;; problem 09 - Sets: conj
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-09/
+
 ;; (= #{1 2 3 4} (conj #{1 4 3} __))
 
 (= #{1 2 3 4} (conj #{1 4 3} 2))
