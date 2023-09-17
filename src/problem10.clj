e4ca2c9fd1e0e81e72db2c0afc137cd5295917e3
sungsik.kim <skystar819@gmail.com>
2023-09-18 05:18:45 +0900

    update problem 10

--- src/problem10.clj
+++ src/problem10.clj
@@ -1,6 +1,8 @@
 (ns problem10)
 
 ;; problem 10 - Intro to Maps
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-10/
+
 ;; (= __ ((hash-map :a 10, :b 20, :c 30) :b))
 ;; (= __ (:b {:a 10, :b 20, :c 30}))
 
