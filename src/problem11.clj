566a43760e250ebce28bc2e3bb1c7f68fa6feb09
sungsik.kim <skystar819@gmail.com>
2023-09-19 05:17:18 +0900

    update problem 11

--- src/problem11.clj
+++ src/problem11.clj
@@ -1,5 +1,7 @@
 (ns problem11)
 
 ;; problem 11 - Maps: conj
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-11/
+
 ;; (= {:a 1, :b 2, :c 3} (conj {:a 1} __ [:c 3]))
 (= {:a 1, :b 2, :c 3} (conj {:a 1} [:b 2] [:c 3]))
