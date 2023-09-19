0d54b4cfbc6c73b47b34a03c871b60d1e5a8b58b
sungsik.kim <skystar819@gmail.com>
2023-09-20 05:47:14 +0900

    update problem 12

--- src/problem12.clj
+++ src/problem12.clj
@@ -1,6 +1,8 @@
 (ns problem12)
 
 ;; problem 12 - Intro to Sequences
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-12/
+
 ;; (= __ (first '(3 2 1)))
 ;; (= __ (second [2 3 4]))
 ;; (= __ (last (list 1 2 3)))
