0b94ddad4f5db648d4b7b0d1b1d07b5c9010bcbe
sungsik.kim <skystar819@gmail.com>
2023-09-21 05:21:01 +0900

    update problem 13

--- src/problem13.clj
+++ src/problem13.clj
@@ -1,5 +1,8 @@
 (ns problem13)
 
 ;; problem 13 - Sequences: rest
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-13/
+
 ;; (= __ (rest [10 20 30 40]))
+
 (= [20 30 40] (rest [10 20 30 40]))
