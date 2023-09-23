f3c8d9fcc3e2cc0d3cd77694fe4b7e222ca00cc4
sungsik.kim <skystar819@gmail.com>
2023-09-24 06:03:47 +0900

    update problem 16

--- src/problem16.clj
+++ src/problem16.clj
@@ -1,6 +1,8 @@
 (ns problem16)
 
 ;; problem 16 - Hello World
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-16/
+
 ;; (= (__ "Dave") "Hello, Dave!")
 ;; (= (__ "Jenn") "Hello, Jenn!")
 ;; (= (__ "Rhea") "Hello, Rhea!")
