cf07f91bd5e9fe6f3b51a3742aeb45a697d33dd5
sungsik.kim <skystar819@gmail.com>
2023-09-25 05:39:13 +0900

    update problem 17

--- src/problem17.clj
+++ src/problem17.clj
@@ -1,5 +1,7 @@
 (ns problem17)
 
 ;; problem 17 - Sequences: map
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-17/
+
 ;; (= __ (map #(+ % 5) '(1 2 3)))
 (= '(6 7 8) (map #(+ % 5) '(1 2 3)))
