329f8b30dea8fc0e978d32956d23eff9190fdf3f
sungsik.kim <skystar819@gmail.com>
2023-09-26 05:45:52 +0900

    update problem 18

--- src/problem18.clj
+++ src/problem18.clj
@@ -1,6 +1,8 @@
 (ns problem18)
 
 ;; problem 18 - Sequences: filter
+;; blug-url : https://sungsik819.github.io/posts/4clojure/problem-18/
+
 ;; (= __ (filter #(> % 5) '(3 4 5 6 7)))
 
 (= '(6 7) (filter #(> % 5) '(3 4 5 6 7)))
